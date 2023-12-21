3905edd6cbaedc40d1416c95b7bfeef37ff1ada6
Shoui-Laptop <dimydeveza@gmail.com>
2023-12-21 19:36:35 -0300

    Adjust: a comment, have to give permissions to the file to run

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# have to chmod +x ./install.sh to work
 
 #  have to auto install vim
-#  ln -s ~/projetos/.dotfiles/nvim ~/.config/nvim -- Creates symlink of nvim configs
-#  ln -s ~/projetos/.dotfiles/.bash_aliases ~/.bash_aliases -- Creates symlink of bash_aliases
+ln -s ~/projetos/.dotfiles/nvim ~/.config/nvim # Creates symlink of nvim configs
+ln -s ~/projetos/.dotfiles/.bash_aliases ~/.bash_aliases # Creates symlink of bash_aliases
 #  have to create bash scripts and symlink
 
