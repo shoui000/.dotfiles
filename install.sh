fa3920bcd6e7574512a0b280925bb438c5276649
Shoui-Laptop <dimydeveza@gmail.com>
2023-12-24 17:10:00 -0300

    Notes: just some notes

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@
 ln -s ~/projetos/.dotfiles/nvim ~/.config/nvim # Creates symlink of nvim configs
 ln -s ~/projetos/.dotfiles/.dotfiles/.bash_aliases ~/.bash_aliases # Creates symlink of bash_aliases
 #  have to create a method of adding bash commands to the .bashrc auto
+#  have to create a metohd of adding $ADVENT to .bashrc
 #  have to create a method of auto chmod every command in the bash commands folder
