9a5caa620d711a1e137df66d0a771294696f1280
shoui <dimydeveza@gmail.com>
2023-12-26 13:31:14 -0300

    Update: install.sh working and nvim updates

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 # have to chmod +x ./install.sh to work
 
-#  have to auto install vim
-ln -s ~/projetos/.dotfiles/nvim ~/.config/nvim # Creates symlink of nvim configs
+sudo snap install nvim --classic -y
+ln -s ~/projetos/.dotfiles/nvim ~/.config # Creates symlink of nvim configs
+
 ln -s ~/projetos/.dotfiles/.dotfiles/.bash_aliases ~/.bash_aliases # Creates symlink of bash_aliases
-#  have to create a method of adding bash commands to the .bashrc auto
-#  have to create a metohd of adding $ADVENT to .bashrc
+
 #  have to create a method of auto chmod every command in the bash commands folder
-#  install fzf
+
+sudo apt install fzf -y
+echo 'export PATH="~/projetos/.dotfiles/bash_commands:$PATH"' >> ~/.bashrc # Add bash commands folder to the path
+echo 'export ADVENT="~/projetos/advent-of-code:$ADVENT"' >> ~/.bashrc # Exports advent of code folder as $ADVENT
+chmod +x -r ~/projetos/.dotfiles/bash_commands
