8891b61d0e8ed2f4e01b8e252b890d7779955e54
Shoui-Laptop <dimydeveza@gmail.com>
2023-12-29 17:34:43 -0300

    indentation

--- nvim/lua/shoui000/lazy.lua
+++ nvim/lua/shoui000/lazy.lua
@@ -1,41 +1,41 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
-	install = {
-		missing = true,
-		colorscheme = {'dracula'}
-	}
+    install = {
+        missing = true,
+        colorscheme = {'dracula'}
+    }
 }
 
 require("lazy").setup({
 
-	{'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
+    {'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
 
     {'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ...},
 
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{"ThePrimeagen/harpoon"},
-	{"mbbill/undotree"},
-	{"tpope/vim-fugitive"},
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {"ThePrimeagen/harpoon"},
+    {"mbbill/undotree"},
+    {"tpope/vim-fugitive"},
 
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 
     {'ThePrimeagen/vim-be-good'},
     {'vim-airline/vim-airline'},
