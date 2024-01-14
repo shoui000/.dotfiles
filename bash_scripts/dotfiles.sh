945b21db550e58efeb4f4caaa05bf4a586eb375c
Shoui-Laptop <dimydeveza@gmail.com>
2024-01-14 17:36:20 -0300

    Tmux config and deleting unused scripts

---
+++ bash_scripts/dotfiles.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd ~/projetos/.dotfiles/
+vim .
--- install.sh
+++ install.sh
@@ -3,13 +3,10 @@
 
 # install tmux, tpm
 sudo snap install nvim --classic -y
-ln -s ~/projetos/.dotfiles/nvim ~/.config # Creates symlink of nvim configs
+sudo apt install fzf -y
 
+ln -s ~/projetos/.dotfiles/nvim ~/.config # Creates symlink of nvim configs
 ln -s ~/projetos/.dotfiles/.dotfiles/.bash_aliases ~/.bash_aliases # Creates symlink of bash_aliases
 
 #  have to create a method of auto chmod every command in the bash commands folder
-
-sudo apt install fzf -y
-echo 'export PATH="~/projetos/.dotfiles/bash_commands:$PATH"' >> ~/.bashrc # Add bash commands folder to the path
-echo 'export ADVENT="~/projetos/advent-of-code:$ADVENT"' >> ~/.bashrc # Exports advent of code folder as $ADVENT
-chmod +x -r ~/projetos/.dotfiles/bash_commands
+chmod +x ~/projetos/.dotfiles/bash_scripts/
