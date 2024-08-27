18dde8aa5b6d1a3b0595e2d98c454288184d60a1
Dimitry Deveza <dimydeveza@gmail.com>
2024-08-27 15:14:01 -0300

    changes: in the end, dont needed these things (alias)

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,24 @@
-set-alias cat        get-content -Scope private
-set-alias clear      clear-host -Scope private
-set-alias h          get-history -Scope private
-set-alias history    get-history -Scope private
-set-alias kill       stop-process -Scope private
-set-alias lp         out-printer -Scope private
-set-alias ls         get-childitem -Scope private
-set-alias mount      new-mshdrive -Scope private
-set-alias mv         move-item -Scope private
-set-alias ps         get-process -Scope private
-set-alias pwd        get-location -Scope private
-set-alias r          invoke-history -Scope private
-set-alias rm         remove-item -Scope private
-set-alias rmdir      remove-item -Scope private
-set-alias cls        clear-host -Scope private
-set-alias chdir      set-location -Scope private
-set-alias erase      remove-item -Scope private
-set-alias rd         remove-item -Scope private
-set-alias ren        rename-item -Scope private
-set-alias set        set-variable -Scope private
-set-alias type       get-content -Scope private
+# set-alias cat        get-content -Scope private
+# set-alias clear      clear-host -Scope private
+# set-alias h          get-history -Scope private
+# set-alias history    get-history -Scope private
+# set-alias kill       stop-process -Scope private
+# set-alias lp         out-printer -Scope private
+# set-alias ls         get-childitem -Scope private
+# set-alias mount      new-mshdrive -Scope private
+# set-alias mv         move-item -Scope private
+# set-alias ps         get-process -Scope private
+# set-alias pwd        get-location -Scope private
+# set-alias r          invoke-history -Scope private
+# set-alias rm         remove-item -Scope private
+# set-alias rmdir      remove-item -Scope private
+# set-alias cls        clear-host -Scope private
+# set-alias chdir      set-location -Scope private
+# set-alias erase      remove-item -Scope private
+# set-alias rd         remove-item -Scope private
+# set-alias ren        rename-item -Scope private
+# set-alias set        set-variable -Scope private
+# set-alias type       get-content -Scope private
 
 function help
 {
