ba1e0ef7028d0c0bbbef73f5df3d0ffc1f9bc2a1
Dimitry Deveza <dimydeveza@gmail.com>
2025-01-30 10:20:24 -0300

    comment: ts_ls errors

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -20,7 +20,7 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'ts_ls', 'rust_analyzer'},
+  ensure_installed = {'ts_ls', 'rust_analyzer'}, -- You need npm to have ts_ls
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({
