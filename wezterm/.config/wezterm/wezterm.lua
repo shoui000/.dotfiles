8a579104a6f923d9b4e5c6e32537356b2efa10b2
Dimitry Deveza <dimydeveza@gmail.com>
2025-02-19 16:13:12 -0300

    Wezterm linux config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,6 +8,13 @@ config.term = 'xterm-256color'
 if wezterm.target_triple:find("windows") ~= nil then
   local winConfig = require 'windows_config'
   winConfig.apply_to_config(config)
+else
+  config.window_decorations = 'RESIZE'
+
+  wezterm.on("gui-startup", function()
+    local tab, pane, window = wezterm.mux.spawn_window{}
+    window:gui_window():maximize()
+  end)
 end
 
 return config
