70137bc941fd77f72d98a4c9f80dd156d20c0e65
Dimitry Deveza <dimydeveza@gmail.com>
2025-07-17 15:15:08 -0300

    changes: moving plugin load order

--- nvim/.config/nvim/lua/shoui000/lazy.lua
+++ nvim/.config/nvim/lua/shoui000/lazy.lua
@@ -20,26 +20,6 @@ local opts = {
 require("lazy").setup({
 
     {'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
-
-    {'vim-airline/vim-airline-themes'},
-
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {"ThePrimeagen/harpoon"},
-    {"mbbill/undotree"},
-    {"tpope/vim-fugitive"},
-
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'rafamadriz/friendly-snippets'},
-
     {
         "christoomey/vim-tmux-navigator",
         cmd = {
@@ -59,6 +39,25 @@ require("lazy").setup({
     },
 
     {'vim-airline/vim-airline'},
+    {'vim-airline/vim-airline-themes'},
+
+    {"ThePrimeagen/harpoon"},
+    {"mbbill/undotree"},
+    {"tpope/vim-fugitive"},
     {'ThePrimeagen/refactoring.nvim'},
+
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'rafamadriz/friendly-snippets'},
 
 }, opts)
